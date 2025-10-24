927c25343fbbf57ab124ead76a3d078272b34732
TravalIndia <travalindia86@gmail.com>
2025-10-24 15:20:21 +0530

    Update github-service.js

--- github-service.js
+++ github-service.js
@@ -5,7 +5,7 @@ class GitHubService {
             username: 'DeepakSingh86',
             repo: 'mountain-life-uttarakhand',
             branch: 'main',
-            token: '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'
+            token: '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'
         };
         this.baseURL = 'https://api.github.com';
     }
@@ -205,3 +205,4 @@ class GitHubService {
 
 // Create global instance
 const githubService = new GitHubService();
+
