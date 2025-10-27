c85a368b18ce203c0efaca6f4d7d87a91e25e474
TravalIndia <travalindia86@gmail.com>
2025-10-27 12:17:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -841,3 +841,4 @@ function formatDisplayDate(dateString) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(dateString).toLocaleDateString(undefined, options);
 }
+
